1a6181fc9bcd8bed5e7b301dbed25e6e1f6d2e4a
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-05 12:59:33 +0000

    Ahora los fetch son dinámicos, trae la alerta cuando el usuario es correcto, pero no trae las tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,44 +3,69 @@ import React, { useState, useEffect, } from "react";
 // Componente funcional Home
 const Home = () => {
 
-
-    const [users, setUsers] = useState([])
     const [newUsers, setNewUsers] = useState("")
-
-   
-
- 
-const acceder = async(event) => {
-    event.preventDefault()
-    const response = await fetch(`https://playground.4geeks.com/todo/docs#/User%20operations/read_user_users__{user_name}__get`)
-    if(response.ok) {
-        const data = await response.json()
-        setUsers(data.todos)}
-     }
+    const [users, setUsers] = useState([])
     
-     
-    const createUser = async(event) => {
-    event.preventDefault()
-    const response1 = await fetch(`https://playground.4geeks.com/todo/users/${name}`,{
-    method : 'POST'})
-    if(response1.ok) {
-        const dataNewUser = await response1.json()
-        setNewUsers([...users, dataNewUser])}     
+    
+
+    useEffect(() => {
+        //codigo que se ejecutará cuando mi estado users cambie
+        access()
+    }, [setNewUsers])
+
+
+
+    useEffect(() => {
+        //codigo que se ejecutará cuando mi estado users cambie
+        setNewUsers("")
+    }, [users])
+
+
+    const access = async (event) => {
+        event.preventDefault()
+        const response = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`)
+        if (response.ok) {
+            const data = await response.json()
+            setUsers(data.todos)
+            alert("Inició sesión con éxito")
+        }
+        else {
+            alert('El usuario no existe, necesita registrarse')
+            createUser()
+        }
+    }
+
+    const createUser = async (event) => {
+        event.preventDefault()
+        const response = await fetch(`https://playground.4geeks.com/todo/docs#/User%20operations/create_user_users__user_${newUsers}__post`, {
+            method: "POST",
+            body: JSON.stringify(todos),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        if (response.ok) {
+            await response.json()
+            setUsers([...users, { label: newUsers, is_done: false }])
+            alert("Se registro nuevo usurario")
+        }
     }
 
 
-useEffect(() => {
-    //codigo que se va a ejcutar ni bien se cargue mi plataforma
-    acceder()
-   
-}, [])
+    const task = async (event) => {
+        event.preventDefault()
+        const response = await fetch(`https://playground.4geeks.com/todo/docs#/User%20operations/read_user_users__user_${newUsers}`, {
+            method: "POST",
+            body: JSON.stringify(todos),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        if (response.ok) {
+            response = await response.json([])
+        }
 
-useEffect(() => {
-    //codigo que se va a ejcutar ni bien se cargue mi plataforma
-   
-    createUser()
-}, [acceder()])
-
+    }
 
 
     const handleKeyPress = (event) => {
@@ -50,66 +75,43 @@ useEffect(() => {
 
     return (
         <div className="container" >
-             {users.map((item, index) => <div key={index}>{item.name}</div>)} 
-            <form onSubmit={acceder} className="d-flex p-4">
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 ">Nombre de Usuario</label>
-                    <input  value={newUsers} type="text" className="form-control" i aria-describedby="emailHelp"   onChange={(e) => setNewUsers(e.target.value)} />
-                </div>
-                <button type="submit" className="btn btn-primary mx-3">Acceder</button>
-            </form>
-
-            <ol className="list-group pt-2">
-                {/* Mapea la lista de tareas para renderizar cada una */}
-                {users.map((item, index) => (
-                    <li key={index}
-
-
-                        className={`list-group-item fs-5 rounded d-flex justify-content-start ${item.is_done ? 'is_done' : ''}`}>
-                  
-                    </li>
-                ))}
-            </ol>
-
-
-
-
-
-
-
-
-
-
-
+            {users.map((item, index) => <div key={index}>{item.todos}</div>)}
 
             <div className="d-flex">
+                <form onSubmit={access} className="d-flex p-4">
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 ">Nombre de Usuario</label>
+                        <input  type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setNewUsers(event.target.value)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary mx-3">Acceder</button>
+                </form>
+
+
 
                 <form onSubmit={createUser} className="d-flex p-4">
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " >Registrar Usuario</label>
-                        <input  type="text" className="form-control" i aria-describedby="emailHelp" />
+                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " > Registrarse</label>
+                        <input  value={newUsers}  type="text" className="form-control" aria-describedby="emailHelp"    onChange={createUser} />
                     </div>
                     <button type="submit" className="btn btn-primary mx-3">Registrarse</button>
                 </form>
 
-
-                <form className="d-flex p-4">
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " >Eliminar Usuario</label>
-                        <input  type="text" className="form-control" i aria-describedby="emailHelp" />
-                    </div>
-                    <button type="submit" className="btn btn-danger mx-3">Eliminar Usuario</button>
-                </form>
             </div>
 
 
 
-          
+            <form onSubmit={task} className="d-flex p-4">
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " >Crear Tarea</label>
+                    <input type="text" className="form-control" aria-describedby="emailHelp" />
+                </div>
+                <button type="submit" className="btn btn-success mx-3">Crear Tarea</button>
+            </form>
 
-              
 
-            </div>
-      
+
+        </div>
+
     );
 };
 
