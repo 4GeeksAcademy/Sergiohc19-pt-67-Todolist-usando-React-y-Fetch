ed4f113fd4fe829677cc5b421bab589efe0565c0
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-09 00:48:14 +0000

    Agrega funcionalidad para traer tareas de un usuario existente al iniciar sesión correctamente, faltan completar las demás funciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,120 +1,147 @@
-import React, { useState, useEffect, } from "react";
+import { string } from "prop-types";
+import React, { useState, useEffect } from "react";
 
-// Componente funcional Home
 const Home = () => {
-
-    const [newUsers, setNewUsers] = useState("")
-    const [users, setUsers] = useState([])
-    
-    
+    const [newUsers, setNewUsers] = useState("");
+    const [users, setUsers] = useState([]);
+    const [registro, setRegistro] = useState("");
 
     useEffect(() => {
-        //codigo que se ejecutará cuando mi estado users cambie
-        access()
-    }, [setNewUsers])
-
+        createUser();
+    }, [setNewUsers]);
 
 
     useEffect(() => {
-        //codigo que se ejecutará cuando mi estado users cambie
-        setNewUsers("")
-    }, [users])
-
+        setNewUsers("");
+    }, [users]);
 
     const access = async (event) => {
-        event.preventDefault()
-        const response = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`)
+        event.preventDefault();
+        const response = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`, {
+
+        })
         if (response.ok) {
-            const data = await response.json()
+            const data = await response.json();
             setUsers(data.todos)
-            alert("Inició sesión con éxito")
+            alert("Inició sesión con éxito");
+            tareas()
+        } else {
+            alert('El usuario no existe, necesita registrarse');
+
+            setNewUsers("");
+            createUser();
         }
-        else {
-            alert('El usuario no existe, necesita registrarse')
-            createUser()
-        }
-    }
+    };
+
+
+
 
     const createUser = async (event) => {
-        event.preventDefault()
-        const response = await fetch(`https://playground.4geeks.com/todo/docs#/User%20operations/create_user_users__user_${newUsers}__post`, {
+        event.preventDefault();
+        const response = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`, {
             method: "POST",
-            body: JSON.stringify(todos),
+            body: JSON.stringify([]),
             headers: {
                 "Content-Type": "application/json"
             }
-        })
+        });
         if (response.ok) {
-            await response.json()
-            setUsers([...users, { label: newUsers, is_done: false }])
-            alert("Se registro nuevo usurario")
+            const data = await response.json();
+            setUsers([...users, data]);
+            alert("Se registró un nuevo usuario");
+            setRegistro("")
+            users([])
         }
-    }
+        else {
+            alert("No se pudo registrar el usuario")
+            setRegistro("")
+        }
+
+    };
 
 
-    const task = async (event) => {
-        event.preventDefault()
-        const response = await fetch(`https://playground.4geeks.com/todo/docs#/User%20operations/read_user_users__user_${newUsers}`, {
+
+
+     const tareas = async (event) => {
+        event.preventDefault();
+        const response = await fetch('https://playground.4geeks.com/todo/todos/Sergiohc.19', {
             method: "POST",
-            body: JSON.stringify(todos),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
+            body: JSON.stringify( label),
+            headers: { "Content-Type": "application/json" }
+        });
         if (response.ok) {
-            response = await response.json([])
+            const data = await response.json();
+            setUsers(data.label);
+            alert("Se creó la tarea correctamente");
+        } else {
+            alert("No se pudo crear la tarea");
         }
+    };    
 
-    }
+
+
+
+    {/* const deleteItem = async (id) => {
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: "DELETE",
+            headers: { 'Accept': 'application/json' }
+        });
+
+        if (response.ok) {
+            setUsers(users.filter(user => user.label !== task.label));
+            alert("Se borró la tarea");
+        } else {
+            alert("No se pudo borrar la tarea");
+        }
+    };
+                                                                */}
 
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
+            
         }
     };
 
     return (
-        <div className="container" >
-            {users.map((item, index) => <div key={index}>{item.todos}</div>)}
+        <div className="container d-flex">
 
-            <div className="d-flex">
-                <form onSubmit={access} className="d-flex p-4">
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 ">Nombre de Usuario</label>
-                        <input  type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setNewUsers(event.target.value)} />
-                    </div>
-                    <button type="submit" className="btn btn-primary mx-3">Acceder</button>
-                </form>
-
-
-
-                <form onSubmit={createUser} className="d-flex p-4">
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " > Registrarse</label>
-                        <input  value={newUsers}  type="text" className="form-control" aria-describedby="emailHelp"    onChange={createUser} />
-                    </div>
-                    <button type="submit" className="btn btn-primary mx-3">Registrarse</button>
-                </form>
-
-            </div>
-
-
-
-            <form onSubmit={task} className="d-flex p-4">
+            <form  className="d-flex p-4">
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5 " >Crear Tarea</label>
-                    <input type="text" className="form-control" aria-describedby="emailHelp" />
+                    <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5">Registrarse</label>
+                    <input  value={registro} type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setRegistro(event.target.value)} />
                 </div>
-                <button type="submit" className="btn btn-success mx-3">Crear Tarea</button>
+                <button onClick={createUser} type="submit" className="btn btn-primary mx-3">Registrarse</button>
             </form>
 
+            <div className="d-flex flex-column">
+                <form onSubmit={access} className="d-flex p-4">
+                    <button onClick={tareas}   className="btn btn-success mx-3">Agregar Tareas</button>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5">Usuario</label>
+                        <input value={newUsers} type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setNewUsers(event.target.value)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary mx-3">Acceder</button>
 
+                </form>
+                {users.map((item) => (
+                    <ul key={item.label}>
+                        <li>
+                            {item.label}
+                            <button
+                                className="eliminar"
+                                onClick={() => deleteItem(item.id)}
+                            >
+                                <i className="fa-solid fa-trash-can fa-lg" style={{ color: "#ff0000" }}></i>
+                            </button>
+                        </li>
+                    </ul>
 
+                ))}
+                <button type="submit" className="btn btn-danger mx-3">Eliminar Lista</button>
+            </div>
         </div>
-
     );
 };
 
-// Exporta el componente Home para su uso en otras partes de la aplicación
 export default Home;
-
