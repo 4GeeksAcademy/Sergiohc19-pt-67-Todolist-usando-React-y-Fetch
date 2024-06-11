7b5ce54f95fa4343d884794755fca3eb98830dc8
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-11 14:59:13 +0000

    El código trae las tareas si el usuario existe y crea el usuario si el usuario no existe

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,14 @@
-import { string } from "prop-types";
+
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
     const [newUsers, setNewUsers] = useState("");
     const [users, setUsers] = useState([]);
-    const [registro, setRegistro] = useState("");
+    
 
     useEffect(() => {
-        createUser();
-    }, [setNewUsers]);
-
+        access()
+    }, [setUsers]);
 
     useEffect(() => {
         setNewUsers("");
@@ -26,31 +25,31 @@ const Home = () => {
             alert("Inició sesión con éxito");
             tareas()
         } else {
+            console.log("adios")
             alert('El usuario no existe, necesita registrarse');
-
+            createUser(newUsers)
             setNewUsers("");
-            createUser();
+            console.log("finalizamos else")
         }
     };
 
-
-
-
-    const createUser = async (event) => {
-        event.preventDefault();
-        const response = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`, {
+    const createUser = async (newUsers) => {
+        console.log("Hola")
+        const response1 = await fetch(`https://playground.4geeks.com/todo/users/${newUsers}`, {
             method: "POST",
-            body: JSON.stringify([]),
+            body: JSON.stringify({
+            }),
             headers: {
                 "Content-Type": "application/json"
             }
         });
-        if (response.ok) {
-            const data = await response.json();
-            setUsers([...users, data]);
+        if (response1.ok) {
+            const data1 = await response1.json();
+            acees(data1)
+            console.log(data1)
             alert("Se registró un nuevo usuario");
             setRegistro("")
-            users([])
+            
         }
         else {
             alert("No se pudo registrar el usuario")
@@ -58,8 +57,6 @@ const Home = () => {
         }
 
     };
-
-
 
 
      const tareas = async (event) => {
@@ -78,45 +75,35 @@ const Home = () => {
         }
     };    
 
-
-
-
-    {/* const deleteItem = async (id) => {
-        const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+    const deleteItem = async () => {
+        const response1 = await fetch(`https://playground.4geeks.com/todo/todos/${newUsers}`, {
             method: "DELETE",
             headers: { 'Accept': 'application/json' }
         });
 
-        if (response.ok) {
-            setUsers(users.filter(user => user.label !== task.label));
+        if (response1.ok) {
+            const data = await response1.json()
+            setUsers(data([]))
             alert("Se borró la tarea");
         } else {
             alert("No se pudo borrar la tarea");
         }
     };
-                                                                */}
-
+                                                                
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
             
         }
     };
-
+ 
     return (
         <div className="container d-flex">
 
-            <form  className="d-flex p-4">
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5">Registrarse</label>
-                    <input  value={registro} type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setRegistro(event.target.value)} />
-                </div>
-                <button onClick={createUser} type="submit" className="btn btn-primary mx-3">Registrarse</button>
-            </form>
+         
 
             <div className="d-flex flex-column">
                 <form onSubmit={access} className="d-flex p-4">
-                    <button onClick={tareas}   className="btn btn-success mx-3">Agregar Tareas</button>
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label fw-bold fs-5">Usuario</label>
                         <input value={newUsers} type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setNewUsers(event.target.value)} />
@@ -130,15 +117,17 @@ const Home = () => {
                             {item.label}
                             <button
                                 className="eliminar"
-                                onClick={() => deleteItem(item.id)}
+                                onClick={() => (item.label)}
                             >
                                 <i className="fa-solid fa-trash-can fa-lg" style={{ color: "#ff0000" }}></i>
                             </button>
                         </li>
+                        
                     </ul>
+                    
 
                 ))}
-                <button type="submit" className="btn btn-danger mx-3">Eliminar Lista</button>
+                <button onClick={deleteItem}   type="submit" className="btn btn-danger mx-3">Eliminar Lista</button>
             </div>
         </div>
     );
